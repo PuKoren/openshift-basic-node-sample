23f8d3b5799be936162c558ff30359be774a3b9e
PuKoren <koren@pukogames.com>
2014-12-15 23:48:06 +0100

    Fixed openshift IP binding

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var http = require('http')
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 http.createServer(function (req, res) {
 	res.writeHead(200);
 	res.end('Hello world');
-}).listen(port);
+}).listen(port, ip);
 
 console.log('Server listening on ', port);
